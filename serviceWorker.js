fd22b07ba67b25c4c458cf1b03887b9891096f93
NonoSoli <nolandski246@gmail.com>
2025-05-14 09:04:37 +0200

    updated

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-const version = "2025-12-05-8"; // <-- à mettre à jour à chaque nouvelle version
+const version = "2025-14-05-1"; // <-- à mettre à jour à chaque nouvelle version
 const staticCacheName = `cache-${version}`;
 const assets = ["/", "https://funlab.be/BeAware//home.html"];
 
